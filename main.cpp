92aa0e29a0ebcc99c4ba5ddf3e75b50d22e5ea6c
Jake Baker <jake.baker@sony.com>
2026-05-17 13:13:09 -0500

    add raylib stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "raylib.h"
+
 int main(void) {
     std::cout << "hello world" << std::endl;
+    InitWindow(800, 450, "raylib example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
